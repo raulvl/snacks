e7e9cf3d00e5d6cd23db132be3339d28d3b1f546
raulvl <raulvillegasleiva@gmail.com>
2016-05-16 00:09:57 -0300

    fixing querys from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,11 +34,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
